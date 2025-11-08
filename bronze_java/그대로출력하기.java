7863800212aa66eed86b24ac4c8253671e92b2ff
seungyoon <neoyoon04@gmail.com>
2025-11-08 21:00:01 +0900

    문제가 너무 이상함.

---
+++ bronze_java/그대로출력하기.java
@@ -0,0 +1,17 @@
+package bronze_java;
+import java.io.*;
+
+public class 그대로출력하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = "";
+
+        while ((s = br.readLine()) != null) {
+            bw.write(s);
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}

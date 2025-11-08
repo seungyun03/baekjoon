453e330d9690dcfa01455680f46c9b688f97e07b
seungyoon <neoyoon04@gmail.com>
2025-11-08 21:01:24 +0900

    문제가 너무 이상함

--- bronze_java/그대로출력하기.java
+++ bronze_java/그대로출력하기.java
@@ -5,9 +5,7 @@ public class 그대로출력하기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         String s = "";
-
         while ((s = br.readLine()) != null) {
             bw.write(s);
             bw.newLine();
